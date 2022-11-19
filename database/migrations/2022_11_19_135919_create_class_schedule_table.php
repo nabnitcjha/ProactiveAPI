1dddcf897d8cab90fe92d6bb6e5505ca30598fe2
Nabnit Jha <nabnit.jha@sofossoftwares.com>
2022-11-20 00:14:25 +0545

    create necessary migration

---
+++ database/migrations/2022_11_19_135919_create_class_schedule_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClassScheduleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('class_schedule', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('topic');
+            $table->integer('teacher_id')->unsigned();
+            $table->integer('subject_id')->unsigned();
+            $table->datetime('start_date');
+            $table->datetime('end_date');
+            $table->longText('zoom_link')->nullable();
+            $table->integer('class_repeat')->default('0');
+            $table->integer('class_complete')->default('0');
+            $table->text('selected_day')->nullable();
+            $table->string('class_unique_id');
+            $table->string('description');
+            $table->timestamps();
+
+            $table->foreign('teacher_id')->references('id')->on('teachers')->onDelete('cascade');
+            $table->foreign('subject_id')->references('id')->on('subjects')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('class_schedule');
+    }
+}
---
+++ database/migrations/2022_11_19_172454_create_parent_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParentStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parent_students', function (Blueprint $table) {
+            $table->integer('student_id')->unsigned();
+            $table->integer('parent_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('parent_id')->references('id')->on('parents')->onDelete('cascade');
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('parent_students');
+    }
+}
